f3d258ba9c00c9d66037285ec8523246b42a8d1a
alhailrose <sfqbgs77@gmail.com>
2025-01-08 12:55:29 +0700

    coba testing

--- src/validation/authenticationValidate.js
+++ src/validation/authenticationValidate.js
@@ -16,32 +16,4 @@ const signinValidate = Joi.object({
   password: Joi.string().required(),
 }).options({ abortEarly: false });
 
-const passwordValidate = Joi.object({
-  oldPassword: Joi.string()
-    .min(8)
-    .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/)
-    .message(
-      "Password must contain letters and numbers only, and be at least 8 characters long"
-    )
-    .required(),
-  newPassword: Joi.string()
-    .min(8)
-    .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/)
-    .message(
-      "Password must contain letters and numbers only, and be at least 8 characters long"
-    )
-    .required(),
-  confirmPassword: Joi.string()
-    .valid(Joi.ref("newPassword"))
-    .messages({
-      "any.only": "Passwords do not match",
-    })
-    .required(),
-}).options({ abortEarly: false });
-
-const userValidate = Joi.object({
-  name: Joi.string().min(3).max(100).required(),
-  email: Joi.string().email().required(),
-}).options({ abortEarly: false });
-
-export { signupValidate, signinValidate, passwordValidate, userValidate };
+export { signupValidate, signinValidate };
