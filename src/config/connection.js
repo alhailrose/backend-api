dcb1c4b6e9786e35b3dd078e171ea604ad111380
alhailrose <sfqbgs77@gmail.com>
2025-01-15 07:37:06 +0700

    coba lagi

--- src/config/connection.js
+++ src/config/connection.js
@@ -5,25 +5,14 @@ dotenv.config();
 
 let dbConfig;
 
-if (process.env.NODE_ENV === "development") {
-  dbConfig = {
-    host: process.env.DEV_DB_HOST,
-    user: process.env.DEV_DB_USER,
-    port: process.env.DEV_DB_PORT,
-    password: process.env.DEV_DB_PASSWORD,
-    database: process.env.DEV_DB_NAME,
-  };
-  console.log("🔧 Connecting to the local database (development)...");
-} else {
-  dbConfig = {
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    user: process.env.PROD_DB_USER,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-  };
-  console.log("🔧 Connecting to the production database...");
-}
+dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
 const dbPool = mysql.createPool(dbConfig);
 
 export default dbPool.promise();
